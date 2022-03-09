a5fe94e1a6ac0abf2e071d932f4ef895dec14491
vimitali368 <vimitali368@gmail.com>
2022-03-09 23:54:13 +0300

    09_j4_02 author statistics

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -54,14 +54,16 @@
                         </p>
                     </a>
                 </li>
-                <li class="nav-item">
-                    <a href="{{ route('admin.statistics.author') }}" class="nav-link">
-                        <i class="nav-icon fas fa-tachometer-alt"></i>
-                        <p>
-                            Статистика
-                        </p>
-                    </a>
-                </li>
+                @if(auth()->user()->hasRole('administrator-user'))
+                    <li class="nav-item">
+                        <a href="{{ route('admin.statistics.author') }}" class="nav-link">
+                            <i class="nav-icon fas fa-tachometer-alt"></i>
+                            <p>
+                                Статистика
+                            </p>
+                        </a>
+                    </li>
+                @endif
             </ul>
         </nav>
         <!-- /.sidebar-menu -->
